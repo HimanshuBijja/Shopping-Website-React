20b210e335a0813fb60331dcb5c8c72c4ca8cfa0
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-05-20 17:03:34 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
+import { AboutSection } from "../components/AboutSection";
 import { NavBar } from "../components/NavBar";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
+import { HeroSection } from "./HeroSection";
 
 export function Home(){
     return (
         <div>
-            <ThemeToggle/>
+            {/* <ThemeToggle/> */}
             <StarBackground/>
             <NavBar/>
+            <main>
+                <HeroSection/>
+                <AboutSection/>
+            </main>
             
         </div>
     )
